2de6400719d3caebffbd1d20cc4810bd5ed7a74a
Thiti-Dev <thiti.mwk.main@gmail.com>
2023-03-22 13:37:03 +0700

    added kv_namespaces

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -4,5 +4,27 @@ declare global {
   interface Env {
     OPEAN_AI_TOKEN: string;
     GPT_MODEL: string;
+    LINE_CHANNEL_ACCESS_TOKEN: string;
+    GPT: any; // KV namespace
+  }
+
+  interface ILineHookBody {
+    destination: string;
+    events: [
+      {
+        type: "message" | "join";
+        message: {
+          type: "text";
+          id: string;
+          text: string;
+        };
+        webhookEventId: string;
+        deliveryContext: any;
+        timestamp: number;
+        source: any;
+        replyToken: string;
+        mode: "active";
+      }
+    ];
   }
 }
