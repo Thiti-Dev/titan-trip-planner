63a9f0ed967fe4d41699d1402abbc90d7591d88e
Thiti-Dev <thiti.mwk.main@gmail.com>
2023-03-22 13:36:43 +0700

    implemented util->replyLineMessage

---
+++ src/utils/line/reply-message.ts
@@ -0,0 +1,22 @@
+export function replyLineMessage(
+  channelToken: string,
+  replyToken: string,
+  message: string
+) {
+  fetch("https://api.line.me/v2/bot/message/reply", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${channelToken}`,
+    },
+    body: JSON.stringify({
+      replyToken: replyToken,
+      messages: [
+        {
+          type: "text",
+          text: message,
+        },
+      ],
+    }),
+  });
+}
